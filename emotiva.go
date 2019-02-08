4f1564f456ccdc04755f1cba04273e825a1b4b1f
Adam Jacob Muller <adam@adam.gs>
2019-02-07 22:26:26 -0500

    refactor, add commands

--- emotiva.go
+++ emotiva.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"net"
 	"os"
@@ -8,41 +9,121 @@ import (
 	"github.com/urfave/cli"
 )
 
-func emotivaControl(c *cli.Context, command string) error {
-	listenAddr, err := net.ResolveUDPAddr("udp", ":7002")
+func emotivaRW(c *cli.Context, command, wPort, rPort string, target interface{}) (string, error) {
+	listenAddr, err := net.ResolveUDPAddr("udp", ":"+rPort)
 	if err != nil {
-		return err
+		return "", err
 	}
-
-	doneChan := make(chan bool)
 
 	listenConn, err := net.ListenUDP("udp", listenAddr)
-	go func(listenConn *net.UDPConn, doneChan chan<- bool) {
-		buf := make([]byte, 1024)
-		n, addr, err := listenConn.ReadFromUDP(buf)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%d from %s: %s\n", n, addr, buf[0:n])
-		doneChan <- true
-	}(listenConn, doneChan)
-
-	conn, err := net.Dial("udp", "10.0.8.41:7002")
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	data := fmt.Sprintf("<?xml version=\"1.0\" encoding=\"utf-8\"?><emotivaControl>%s</emotivaControl>\n", command)
+	conn, err := net.Dial("udp", c.GlobalString("address")+":"+wPort)
+	if err != nil {
+		return "", err
+	}
+
+	data := fmt.Sprintf("<?xml version=\"1.0\" encoding=\"utf-8\"?>%s", command)
 
 	n, err := conn.Write([]byte(data))
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	fmt.Printf("wrote %d bytes: %s\n", n, data)
 
-	<-doneChan
+	buf := make([]byte, 1024)
+	n, addr, err := listenConn.ReadFromUDP(buf)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("%d from %s: %s\n", n, addr, buf[0:n])
+
+	if target != nil {
+		err = xml.Unmarshal(buf[0:n], target)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return string(buf[0:n]), nil
+}
+
+type EmotivaTransponder struct {
+	XMLName      xml.Name                  `xml:"emotivaTransponder"`
+	Model        string                    `xml:"model"`
+	DataRevision string                    `xml:"dataRevision"`
+	Name         string                    `xml:"name"`
+	Control      EmotivaTransponderControl `xml:"control"`
+}
+
+type EmotivaTransponderControl struct {
+	Version         string `xml:"version"`
+	ControlPort     int    `xml:"controlPort"`
+	NotifyPort      int    `xml:"notifyPort"`
+	InfoPort        int    `xml:"infoPort"`
+	SetupPortTCP    int    `xml:"setupPortTCP"`
+	MenuNotifyPort  int    `xml:"menuNotifyPort"`
+	SetupXMLVersion int    `xml:"setupXMLVersion"`
+}
+
+func emotivaPing(c *cli.Context) error {
+	var response EmotivaTransponder
+
+	_, err := emotivaRW(c, "<emotivaPing/>", "7000", "7001", &response)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%#v\n", response)
+
 	return nil
+}
+
+func emotivaControl(c *cli.Context, command string) error {
+	_, err := emotivaRW(c, fmt.Sprintf("<emotivaControl>%s</emotivaControl>", command), "7002", "7002", nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func inputGenerator() []cli.Command {
+	var inputs []cli.Command
+
+	for _, input := range []string{
+		"hdmi1",
+		"hdmi2",
+		"hdmi3",
+		"hdmi4",
+		"hdmi5",
+		"hdmi6",
+		"hdmi7",
+		"hdmi8",
+		"coax1",
+		"coax2",
+		"coax3",
+		"coax4",
+		"optical1",
+		"optical2",
+		"optical3",
+		"optical4",
+		"ARC",
+	} {
+		inputs = append(inputs, cli.Command{
+			Name:  input,
+			Usage: "select input " + input,
+			Action: func(c *cli.Context) error {
+				return emotivaControl(c, fmt.Sprintf(`<%s value="0" ack="yes" />`, input))
+			},
+		})
+
+	}
+
+	return inputs
 }
 
 func main() {
@@ -50,8 +131,10 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "address",
-			Usage: "Emotiva address",
+			Name:   "address",
+			Value:  "10.0.8.41",
+			Usage:  "Emotiva address",
+			EnvVar: "EMOTIVA_ADDRESS",
 		},
 	}
 
@@ -73,6 +156,48 @@ func main() {
 			Action: func(c *cli.Context) error {
 				return emotivaControl(c, `<info value="0" ack="yes" />`)
 			},
+		},
+		{
+			Name:  "ping",
+			Usage: "emotivaPing",
+			Flags: []cli.Flag{},
+			Action: func(c *cli.Context) error {
+				return emotivaPing(c)
+			},
+		},
+		{
+			Name:  "power",
+			Usage: "set power state",
+			Flags: []cli.Flag{},
+			Subcommands: []cli.Command{
+				{
+					Name:  "on",
+					Usage: "turn power on",
+					Action: func(c *cli.Context) error {
+						return emotivaControl(c, `<power_on value="0" ack="yes" />`)
+					},
+				},
+				{
+					Name:  "off",
+					Usage: "turn power off",
+					Action: func(c *cli.Context) error {
+						return emotivaControl(c, `<power_off value="0" ack="yes" />`)
+					},
+				},
+				{
+					Name:  "standby",
+					Usage: "set standby",
+					Action: func(c *cli.Context) error {
+						return emotivaControl(c, `<standby value="0" ack="yes" />`)
+					},
+				},
+			},
+		},
+		{
+			Name:        "input",
+			Usage:       "set input",
+			Flags:       []cli.Flag{},
+			Subcommands: inputGenerator(),
 		},
 		{
 			Name:  "power",
